ed1fb21c599e8f51e1fe392b872912ecec622081
Matias Rodriguez <mati.rodriguez1201@gmail.com>
2025-04-28 18:16:41 -0300

    Se modifica la barra de navegación.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaHome, FaBuilding, FaMapMarkerAlt, FaUsers, FaPhoneAlt } from 'react-icons/fa';
 
-// Header con logo personalizado y menú de navegación
 const Header = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
   return (
     <header className="header">
       <div className="header-content">
-        {/* Logo imagen inmobiliaria.png */}
         <Link to="/" className="logo-link" aria-label="Inicio">
           <img src="/inmobiliaria.png" alt="Inmobiliaria Logo" className="logo" />
           <span className="title">Inmobiliaria</span>
         </Link>
+        
         <nav className="nav-menu">
-          <Link to="/" className="nav-link">Home</Link>
-          <Link to="/contacto" className="nav-link">Contacto</Link>
+          <div 
+            className="nav-item-wrapper"
+            onMouseEnter={() => setShowDropdown(true)}
+            onMouseLeave={() => setShowDropdown(false)}
+          >
+            <Link to="/propiedades" className="nav-link">
+              <FaHome className="inline-block mr-1" />
+              Propiedades
+            </Link>
+            {showDropdown && (
+              <div className="dropdown-menu">
+                <Link to="/propiedades/comprar" className="dropdown-item">Comprar</Link>
+                <Link to="/propiedades/alquilar" className="dropdown-item">Alquilar</Link>
+                <Link to="/propiedades/inversiones" className="dropdown-item">Inversiones</Link>
+              </div>
+            )}
+          </div>
+          
+          <Link to="/oficinas" className="nav-link">
+            <FaBuilding className="inline-block mr-1" />
+            Oficinas
+          </Link>
+          
+          <Link to="/zonas" className="nav-link">
+            <FaMapMarkerAlt className="inline-block mr-1" />
+            Zonas
+          </Link>
+          
+          <Link to="/agentes" className="nav-link">
+            <FaUsers className="inline-block mr-1" />
+            Agentes
+          </Link>
+          
+          <Link to="/contacto" className="nav-link">
+            <FaPhoneAlt className="inline-block mr-1" />
+            Contacto
+          </Link>
         </nav>
       </div>
     </header>
